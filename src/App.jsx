c1dced534fbd93b57d2153f033d4608b101f604a
pantpad <138708449+pantpad@users.noreply.github.com>
2024-02-19 22:39:56 +0100

    Add basic layout + component structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,30 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Dequizz the bomb</h1>
+      <h2 className="title">Are you a pro? Click to find out</h2>
+      <main className="bomb">
+        <div className="question">
+          <div className="initial-screen">
+            <h2>DEQUIZZ</h2>
+            <p>the</p>
+            <h2>BOMB</h2>
+          </div>
+        </div>
+        <div className="timer">
+          <div className="progress-wrapper">
+            <progress value={50} max={100} />
+            <p className="time-remaining">00:45:00</p>
+          </div>
+        </div>
+        <section className="answers-wrapper">
+          <div className="answers">
+            <button className="correct"></button>
+            <button className="incorrect"></button>
+            <button></button>
+            <button></button>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
