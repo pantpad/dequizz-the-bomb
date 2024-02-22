20c594d10ddbfbd58bc5cc6d55d2c797d6e10c73
pantpad <138708449+pantpad@users.noreply.github.com>
2024-02-22 17:17:04 +0100

    Improved useEffect performance on timer

--- src/components/BombProgress.jsx
+++ src/components/BombProgress.jsx
@@ -5,41 +5,32 @@ export default function BombProgress({ timer, max, removeTime, endGame }) {
   const bombTick = useRef();
 
   useEffect(() => {
-    play();
-  }, []);
-
-  function wait() {
-    setTimeout(play, 1010);
-  }
-
-  function play() {
-    bombTick.current.play();
-    bombTick.current.onEnded = wait();
-  }
+    if (timer < 0 || timer >= 75000) {
+      endGame();
+    }
+  }, [timer, endGame]);
 
   useEffect(() => {
+    const bombTickInterval = setInterval(() => {
+      bombTick.current.play();
+    }, 1008);
+
     const timerI = setInterval(() => {
       removeTime(1000);
     }, 1000);
 
-    if (timer < 0 || timer >= 75000) {
-      endGame();
-    }
-
     return () => {
+      console.log("unmount");
       clearInterval(timerI);
+      clearInterval(bombTickInterval);
     };
-  }, [timer, removeTime, endGame]);
+  }, [removeTime]);
 
   return (
     <>
       <progress value={timer} max={max} />
       <p className="time-remaining">{timer / 1000}</p>
-      <audio
-        ref={bombTick}
-        src="./audio/singlebombtick.mp3"
-        preload="auto"
-      ></audio>
+      <audio ref={bombTick} src="./audio/singlebombtick.mp3" autoPlay></audio>
     </>
   );
 }
